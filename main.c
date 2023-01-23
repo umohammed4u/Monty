69043190e401f17135600c8bc1b3c51189f35d91
umohammed4u <umohammed4u@gmail.com>
2023-01-23 12:18:51 -0800

    main

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include "monty.h"
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
 * main - monty code interpreter
 * @argc: number of arguments
@@ -11,12 +9,10 @@ bus_t bus = {NULL, NULL, NULL, 0};
 int main(int argc, char *argv[])
 {
 	char *content;
-
 	FILE *file;
 	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-
 	unsigned int counter = 0;
 
 	if (argc != 2)
@@ -24,9 +20,9 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fpen(argv[1], "r");
+	file = fopen(argv[1], "r");
 	bus.file = file;
-	if (!file
+	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
@@ -37,14 +33,12 @@ int main(int argc, char *argv[])
 		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
-
 		if (read_line > 0)
 		{
 			execute(content, &stack, counter, file);
 		}
 		free(content);
 	}
-
 	free_stack(stack);
 	fclose(file);
 return (0);
